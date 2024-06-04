08abed24576340fb3688856919e4d609499f130d
samisalehsaeed <samisalehsaeed05@gmail.com>
2024-06-04 14:10:45 +0100

    Sami Saeed

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,6 +1,24 @@
 class Scrabble {
-  // Write your implementation here
+  constructor(word) {
+    this.word = word ? word.trim().toUpperCase() : '';
+  }
+  static letterValues() {
+    return {
+      'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1,
+      'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8,
+      'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1,
+      'P': 3, 'Q': 10, 'R': 1, 'S': 1, 'T': 1,
+      'U': 1, 'V': 4, 'W': 4, 'X': 8, 'Y': 4,
+      'Z': 10
+    };
+  }
 
+  score() {
+    if (!this.word) return 0;
+
+    const values = Scrabble.letterValues();
+    return [...this.word].reduce((acc, letter) => acc + (values[letter] || 0), 0);
+  }
 }
 
 module.exports = Scrabble
