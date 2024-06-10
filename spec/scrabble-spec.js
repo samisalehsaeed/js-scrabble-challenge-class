b7f95c4dad05885ce3cba3a8bd8c966460fefded
samisalehsaeed <samisalehsaeed05@gmail.com>
2024-06-10 15:21:00 +0100

    Sami Saeed

--- spec/scrabble-spec.js
+++ spec/scrabble-spec.js
@@ -1,53 +1,53 @@
 // Note: you shouldn't need to change anything in this file.
 
-Scrabble = require('../src/scrabble')
-let scrabble;
+const Scrabble = require('../src/scrabble')
+let scrabble
 
-describe("Scrabble", function() {
-  it('returns 0 for empty words', function() {
+describe('Scrabble', function () {
+  it('returns 0 for empty words', function () {
     scrabble = new Scrabble('')
 
     expect(scrabble.score()).toEqual(0)
   })
 
-  it('returns 0 for whitespace', function() {
-    scrabble = new Scrabble(" \t\n")
+  it('returns 0 for whitespace', function () {
+    const scrabble = new Scrabble(' \t\n')
 
     expect(scrabble.score()).toEqual(0)
   })
 
-  it('returns 0 for null', function() {
-    scrabble = new Scrabble(null)
+  it('returns 0 for null', function () {
+    const scrabble = new Scrabble(null)
 
     expect(scrabble.score()).toEqual(0)
   })
 
-  it('scores short word', function() {
-    scrabble = new Scrabble('a')
+  it('scores short word', function () {
+    const scrabble = new Scrabble('a')
 
     expect(scrabble.score()).toEqual(1)
   })
 
-  it('scores short word', function() {
-    scrabble = new Scrabble('f')
+  it('scores short word', function () {
+    const scrabble = new Scrabble('f')
 
     expect(scrabble.score()).toEqual(4)
   })
 
-  it('scores a simple word', function() {
-    scrabble = new Scrabble('street')
+  it('scores a simple word', function () {
+    const scrabble = new Scrabble('street')
 
     expect(scrabble.score()).toEqual(6)
   })
 
-  it('scores a more complicated word', function() {
-    scrabble = new Scrabble('quirky')
+  it('scores a more complicated word', function () {
+    const scrabble = new Scrabble('quirky')
 
     expect(scrabble.score()).toEqual(22)
   })
 
-  it('scores a case-insensitive word', function() {
-    scrabble = new Scrabble('OXYPHENBUTAZONE')
+  it('scores a case-insensitive word', function () {
+    const scrabble = new Scrabble('OXYPHENBUTAZONE')
 
     expect(scrabble.score()).toEqual(41)
   })
